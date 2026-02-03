d0512dbd38908c025dbce09cf875d551517477f8
Crys Carmona <crysc4rmon4@gmail.com>
2026-02-03 09:50:54 +0000

    configurado jwt y token de espiracion

--- src/app.py
+++ src/app.py
@@ -1,54 +1,79 @@
 """
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+This module takes care of starting the API Server,
+Loading the DB and adding all global configurations
 """
+
 import os
+from datetime import timedelta
+
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import JWTManager  # 🔐 JWT engine
+
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 
-# from models import Person
+
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../dist/')
+
+static_file_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    '../dist/'
+)
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
+
 db_url = os.getenv("DATABASE_URL")
+
 if db_url is not None:
+    
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
-        "postgres://", "postgresql://")
+        "postgres://", "postgresql://"
+    )
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
-# add the admin
-setup_admin(app)
 
-# add the admin
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
+
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(
+    minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES", 30))
+)
+
+
+jwt = JWTManager(app)
+
+
+# coniguracion de panel de administracin 
+setup_admin(app)
 setup_commands(app)
 
-# Add all endpoints form the API with a "api" prefix
+
+# Organizacion de rutas y direcciones
 app.register_blueprint(api, url_prefix='/api')
 
-# Handle/serialize errors like a JSON object
 
-
+# Bloque para organizar respuestas de error si algo sale mal
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 
+
+# Sitemap y Rutas de front
 
 @app.route('/')
 def sitemap():
@@ -56,17 +81,18 @@ def sitemap():
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = 'index.html'
+
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
 
-# this only runs if `$ python src/main.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
